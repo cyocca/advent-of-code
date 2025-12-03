13ed30e64ab2e31090a5b5dd9967c002444e4664
Chandler Yocca <cey10@pitt.edu>
2025-12-02 21:03:23 -0500

    2025 Day 2 part 1

---
+++ python/adventofcode/2025/day02/solution.py
@@ -0,0 +1,51 @@
+from collections.abc import Iterable
+from adventofcode.utils import load_input
+from dataclasses import dataclass
+from typing_extensions import Self
+
+@dataclass
+class Range:
+
+    start: int
+    end: int
+
+    @classmethod
+    def parse(cls, token: str) -> Self:
+        return cls(*token.split("-"))
+
+def get_numbers(length: int) -> Iterable[int]:
+    if length <= 0:
+        return []
+
+    if length == 1:
+       return range(10)
+
+    start = int("9" * (length - 1)) + 1
+    end = int("9" * length) + 1
+
+    return range(start, end)
+
+def part1() -> int:
+    # General idea: Generate all valid numbers with length half of the starting point,
+    # then repeat once and see if it's in the range. It would be too expensive to
+    # generate the entire range and check each number.
+    ranges = [Range.parse(t) for t in load_input().split(",")]
+    answer = 0
+
+    for r in ranges:
+        # It's not enough to just use the start length.
+        # Consider 998-1012 which has the answer 1010.
+        # We don't want to waste work using the same length twice (for adjacent numbers).
+        lengths = {
+            l // 2
+            for l in range(len(r.start), len(r.end) + 1)
+        }
+        for length in lengths:
+            for number in get_numbers(length):
+                candidate = int(f"{number}{number}")
+                if int(r.start) <= candidate <= int(r.end):
+                    answer += candidate
+
+    return answer
+
+print(part1())
